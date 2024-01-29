dfed6d611dd0ee81d234cd52ed7b950a9f446567
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2024-01-29 12:08:49 +0700

    create project base

---
+++ app/src/main/java/fpoly/md05/appduanmd05/View/Account/SignUpActivity.java
@@ -0,0 +1,17 @@
+package fpoly.md05.appduanmd05.View.Account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fpoly.md05.appduanmd05.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        //create sign up activity
+    }
+}
