c6e330c5ee24a6bb135654ca47ed5d85fad1f05c
Đào Ngọc Sơn <sondnph203712>
2024-01-31 17:53:00 +0700

    son update ui

--- app/src/main/java/fpoly/md05/appduanmd05/MainActivity.java
+++ app/src/main/java/fpoly/md05/appduanmd05/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //develop branch fafafafffd
+        //develop branch fafafafffdfaffa
     }
 }
