0f66ec3bbfd23085da8e6372efeaf7813c58128c
khanh3403 <bnkhanh3403@gmail.com>
2024-02-02 00:10:40 +0700

    khanh_fixdk

--- app/src/main/java/fpoly/md05/appduanmd05/View/Account/SignUpActivity.java
+++ app/src/main/java/fpoly/md05/appduanmd05/View/Account/SignUpActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,6 +23,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,18 +36,13 @@ import fpoly.md05.appduanmd05.R;
 
 public class SignUpActivity extends AppCompatActivity {
     EditText name,sdt,email,pass,rePass;
+    TextView signIn;
     Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        //create sign up activity
-        name=findViewById(R.id.signUp_username);
-        sdt=findViewById(R.id.signUp_Phone);
-        email=findViewById(R.id.signUp_Email);
-        pass=findViewById(R.id.signUp_Pass);
-        rePass=findViewById(R.id.signUp_RePass);
-        signUp=findViewById(R.id.signUp_Register);
+     anhXaView();
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,12 +55,14 @@ public class SignUpActivity extends AppCompatActivity {
                 //validate
                 List<EditText> editTextList = Arrays.asList(name, sdt, email, pass, rePass);
                 List<String> errorMessages = new ArrayList<>();
-
+                String password = pass.getText().toString();
+                String confirmPassword = rePass.getText().toString();
                 for (EditText editText : editTextList) {
                     if (editText.getText().toString().isEmpty()) {
                         errorMessages.add("Vui lòng nhập đầy đủ thông tin cho " + editText.getHint().toString());
                         setRedBorderAndError(editText, "Vui lòng nhập đầy đủ thông tin");
                     }
+
                 }
 
                 if (!errorMessages.isEmpty()) {
@@ -81,16 +81,30 @@ public class SignUpActivity extends AppCompatActivity {
                     errorMessages.add("Email không hợp lệ");
                     setRedBorderAndError(email, "Vui lòng nhập đúng định dạng email");
                 }
+                if(pass.getText().length()<6){
+                    errorMessages.add("Mật khẩu không hợp lệ");
+                    setRedBorderAndError(pass, "Vui lòng nhập trên 6 ký tự");
+                }
                 //validate pass
-                String password = pass.getText().toString();
-                String confirmPassword = rePass.getText().toString();
+
 
                 if (!password.equals(confirmPassword)) {
                     errorMessages.add("Mật khẩu và xác nhận mật khẩu không khớp");
                     setRedBorderAndError(pass, "Mật khẩu không khớp");
                     setRedBorderAndError(rePass, "Mật khẩu không khớp");
                 }
-                onclickSignUp();
+                if(!name.getText().toString().isEmpty()&&isValidPhoneNumber(sdt.getText().toString())
+                        &&isValidEmail(email.getText().toString())&&password.equals(confirmPassword)
+                &&!pass.getText().toString().isEmpty()&&!rePass.getText().toString().isEmpty()){
+                    onclickSignUp();
+                }
+            }
+        });
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+
             }
         });
     }
@@ -104,9 +118,7 @@ public class SignUpActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Intent intent=new Intent(SignUpActivity.this,SignInActivity.class);
-                            startActivity(intent);
-                            finishAffinity();
+                            signIn();
                         } else {
                             // If sign in fails, display a message to the user.
 
@@ -116,7 +128,20 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
 
-
-
+    void anhXaView(){
+        //create sign up activity
+        name=findViewById(R.id.signUp_username);
+        sdt=findViewById(R.id.signUp_Phone);
+        email=findViewById(R.id.signUp_Email);
+        pass=findViewById(R.id.signUp_Pass);
+        rePass=findViewById(R.id.signUp_RePass);
+        signUp=findViewById(R.id.signUp_Register);
+        signIn=findViewById(R.id.signUp_Login);
+    }
+    void signIn(){
+        Intent intent=new Intent(SignUpActivity.this,SignInActivity.class);
+        startActivity(intent);
+        finishAffinity();
+    }
 
 }
