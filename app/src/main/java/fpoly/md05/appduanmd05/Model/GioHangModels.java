67d59622821d8ea33ad212145892c10f687c85af
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2024-03-10 17:31:46 +0700

    hàm thêm sản phẩm vào giỏ hàng ( addcart )

--- app/src/main/java/fpoly/md05/appduanmd05/Model/GioHangModels.java
+++ app/src/main/java/fpoly/md05/appduanmd05/Model/GioHangModels.java
@@ -1,6 +1,15 @@
 package fpoly.md05.appduanmd05.Model;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import fpoly.md05.appduanmd05.Presenter.IGioHang;
 
@@ -33,5 +42,62 @@ public class GioHangModels {
     public GioHangModels(String id_sanpham, long soluong) {
         this.id_sanpham = id_sanpham;
         this.soluong = soluong;/////////////
+    }
+
+    public void AddCart(String idsp) {
+        db.collection("GioHang")
+                .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .collection("ALL")
+                .whereEqualTo("id_sanpham", idsp)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (queryDocumentSnapshots.size() != 0) {
+                            if (queryDocumentSnapshots.size() > 0) {
+                                for (QueryDocumentSnapshot d : queryDocumentSnapshots) {
+                                    long soluong_sp = d.getLong("soluong");
+                                    if (soluong_sp > 0) {
+                                        soluong_sp += 1;
+                                        db.collection("GioHang")
+                                                .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                .collection("ALL")
+                                                .document(d.getId())
+                                                .update("soluong", soluong_sp)
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            callback.OnSucess();
+                                                        } else {
+                                                            callback.OnFail();
+                                                        }
+                                                    }
+                                                });
+                                    }
+                                }
+                            }
+                        } else {
+                            GioHangModels gioHangModels = new GioHangModels(idsp, 1);
+                            db.collection("GioHang")
+                                    .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .collection("ALL")
+                                    .add(gioHangModels)
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                                            if (task.isSuccessful()) {
+                                                callback.OnSucess();
+                                            } else {
+                                                callback.OnFail();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+    }
+
+    public void HandlegetDataGioHang() {
     }
 }
