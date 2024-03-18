873a7b1cd065006f66e0fce0334777e67e1ff1f6
khanh3403 <bnkhanh3403@gmail.com>
2024-03-18 14:48:40 +0700

    fix ctsp

--- app/src/main/java/fpoly/md05/appduanmd05/View/FragMent/FragMent_ProFile.java
+++ app/src/main/java/fpoly/md05/appduanmd05/View/FragMent/FragMent_ProFile.java
@@ -264,7 +264,7 @@ public class FragMent_ProFile extends Fragment implements View.OnClickListener {
                                 }
 
                             dialog.cancel();
-                        }//12345
+                        }//1234
                     }
                     });
                 } else {
