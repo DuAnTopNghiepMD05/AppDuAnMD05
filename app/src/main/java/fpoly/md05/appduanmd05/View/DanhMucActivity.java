d9449bae98d673ed087331496252a1342cf19831
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2024-03-20 19:51:03 +0700

    update danhmuc

--- app/src/main/java/fpoly/md05/appduanmd05/View/DanhMucActivity.java
+++ app/src/main/java/fpoly/md05/appduanmd05/View/DanhMucActivity.java
@@ -1,13 +1,19 @@
 package fpoly.md05.appduanmd05.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -21,12 +27,12 @@ import fpoly.md05.appduanmd05.Model.SanPhamModels;
 import fpoly.md05.appduanmd05.Presenter.SanPhamView;
 import fpoly.md05.appduanmd05.R;
 
-public class DanhMucActivity extends AppCompatActivity  {
-
-    private FirebaseFirestore db;
+public class DanhMucActivity extends AppCompatActivity {
 
     private TextView tvQuan, tvAo, tvKhac;
-    private ArrayList<SanPhamModels> arrayList;
+
+    private boolean isTextViewHidden = false;
+    private ImageView btnback;
     private RecyclerView recyclerView;
     private SanPhamAdapter sanPhamAdapter;
 
@@ -39,18 +45,22 @@ public class DanhMucActivity extends AppCompatActivity  {
         tvQuan = findViewById(R.id.danhmuc_tvQuan);
         tvKhac = findViewById(R.id.danhmuc_tvKhac);
         recyclerView = findViewById(R.id.rcvDanhMuc);
+        btnback = findViewById(R.id.ic_back_dm);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
         tvAo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getDataFromFirestore("Ao");
+                getDataFromFirestore("Áo");
+                hideTextViews();
             }
         });
 
         tvQuan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getDataFromFirestore("Quan");
+                getDataFromFirestore("Quần");
+                hideTextViews();
             }
         });
 
@@ -58,12 +68,36 @@ public class DanhMucActivity extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
                 getDataFromFirestore("Khác");
+                hideTextViews();
             }
         });
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isTextViewHidden) {
+                    Intent intent = new Intent(DanhMucActivity.this, DanhMucActivity.class);
+                    startActivity(intent);
+                    finish(); // Kết thúc activity hiện tại để tránh quay lại nó khi nhấn nút back trên màn hình Danh Mục
+                } else {
+                    onBackPressed(); // Quay về màn hình Home
+                }
+            }
+        });
+
+
+    }
+    private void hideTextViews() {
+        tvAo.setVisibility(View.GONE);
+        tvQuan.setVisibility(View.GONE);
+        tvKhac.setVisibility(View.GONE);
+        isTextViewHidden = true;
     }
 
     private void getDataFromFirestore(String loaiSP) {
-        // Ví dụ:
+        tvAo.setVisibility(View.GONE);
+        tvQuan.setVisibility(View.GONE);
+        tvKhac.setVisibility(View.GONE);
+        Log.d("FirestoreQuery", "Querying Firestore for product type: " + loaiSP);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("SanPham")
                 .whereEqualTo("loaisp", loaiSP)
@@ -71,14 +105,23 @@ public class DanhMucActivity extends AppCompatActivity  {
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        Log.d("FirestoreQuery", "Query successful. Retrieved " + queryDocumentSnapshots.size() + " documents.");
                         ArrayList<SanPhamModels> sanPhamList = new ArrayList<>();
                         for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
                             SanPhamModels sanPham = document.toObject(SanPhamModels.class);
                             sanPhamList.add(sanPham);
                         }
-                        sanPhamAdapter = new SanPhamAdapter((Context) DanhMucActivity.this, sanPhamList, 0);
+                        // Tạo adapter và thiết lập cho RecyclerView
+                        sanPhamAdapter = new SanPhamAdapter(DanhMucActivity.this, sanPhamList, 0);
                         recyclerView.setAdapter(sanPhamAdapter);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("FirestoreQuery", "Error querying Firestore: " + e.getMessage());
                     }
                 });
     }
+
 }
